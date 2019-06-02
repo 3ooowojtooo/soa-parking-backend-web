3b7d479a65eea351e36d04d832a67e1114c1a2f1
Wojciech Góralczyk <goralczyk4@gmail.com>
2019-06-02 21:39:25 +0200

    External rest service module added.

---
+++ external-rest/src/main/java/com/qaury/soa/parking/backend/web/externalrest/ExternalRestApplication.java
@@ -0,0 +1,25 @@
+package com.qaury.soa.parking.backend.web.externalrest;
+
+import com.qaury.soa.parking.backend.web.externalrest.controller.TestController;
+
+import javax.annotation.PostConstruct;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationPath("/")
+public class ExternalRestApplication extends Application {
+
+    private Set<Object> singletons = new HashSet<>();
+
+    @PostConstruct
+    public void init() {
+        singletons.add(new TestController());
+    }
+
+    @Override
+    public Set<Object> getSingletons() {
+        return singletons;
+    }
+}
