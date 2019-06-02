f8462e62cb8c8534e5fd93a640c0e51d3ec94b92
Wojciech Góralczyk <goralczyk4@gmail.com>
2019-06-02 14:54:33 +0200

    Gitignore update. Added IBaseDAO for testing purposes.

---
+++ database/database-api/src/main/java/com/qaury/soa/parking/backend/web/database/api/dao/IBaseDAO.java
@@ -0,0 +1,16 @@
+package com.qaury.soa.parking.backend.web.database.api.dao;
+
+import java.util.List;
+
+public interface IBaseDAO<T> {
+
+    T persist(T entity);
+
+    T edit(T entity);
+
+    T remove(T entity);
+
+    List<T> findAll();
+
+    T find(Integer id);
+}
