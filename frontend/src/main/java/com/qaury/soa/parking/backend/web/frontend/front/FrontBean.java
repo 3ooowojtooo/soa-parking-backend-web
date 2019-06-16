05689a87b3afbde5b29eb17c793d688c89ce3c4c
kurocho <miugiel@gmail.com>
2019-06-16 03:08:52 +0200

    Some basic front working

--- frontend/src/main/java/com/qaury/soa/parking/backend/web/frontend/front/FrontBean.java
+++ frontend/src/main/java/com/qaury/soa/parking/backend/web/frontend/front/FrontBean.java
@@ -1,9 +1,18 @@
 package com.qaury.soa.parking.backend.web.frontend.front;
 
+import com.qaury.soa.parking.backend.web.database.api.entity.Zone;
+import com.qaury.soa.parking.backend.web.database.api.exception.PasswordChangeException;
+import org.primefaces.context.RequestContext;
+
 import javax.ejb.EJB;
 import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.ManagedProperty;
+import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
+import java.util.List;
+import java.util.Map;
+
 
 @Named
 @ApplicationScoped
@@ -20,6 +29,20 @@ public class FrontBean {
         return FacesContext.getCurrentInstance().getExternalContext().isUserInRole("Admin");
     }
 
+    public List<Zone> getZones(){
+        return testEJB.getZonesForLoggedUser();
+    }
+
+    public void notification(){
+        Map<String,String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+        String message = params.get("msg");
+        if(message!=null) {
+            FacesMessage msg = new FacesMessage(message);
+            FacesContext.getCurrentInstance().addMessage(null, msg);
+            RequestContext.getCurrentInstance().update("msgs");
+        }
+    }
+
     public boolean shouldControllerChannelBeRendered() {
         return FacesContext.getCurrentInstance().getExternalContext().isUserInRole("Controller");
     }
@@ -27,4 +50,23 @@ public class FrontBean {
     public String getUserPrincipal() {
         return FacesContext.getCurrentInstance().getExternalContext().getUserPrincipal().getName();
     }
+
+    public void changePassword(String login, String password) {
+        FacesMessage msg = new FacesMessage();
+        System.out.println(login + ":" + password);
+        try {
+            testEJB.changePassword(login, password);
+            msg = new FacesMessage("Changed password for ", login);
+        } catch (PasswordChangeException e) {
+            msg = new FacesMessage("Could not change password: ", e.getMessage());
+        } finally {
+            FacesContext.getCurrentInstance().addMessage(null, msg);
+        }
+    }
+
+
+    public List<String> getLoginsForWhichLoggedUserCanChangePassword() {
+        return testEJB.getLoginsForWhichLoggedUserCanChangePassword();
+    }
+
 }
